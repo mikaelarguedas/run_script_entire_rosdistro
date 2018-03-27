cebd4fc46c309b0d0089cf3331dea610d2cc108a
Mikael Arguedas <mikael@osrfoundation.org>
2018-03-26 23:35:34 -0700

    update run script and dockerfile to mount ssh and gitconfig

--- run_update_downstream.sh
+++ run_update_downstream.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run -ti update_downstream
+docker run --rm \
+  -v $HOME/.ssh:/root/.ssh:ro \
+  -v $HOME/.gitconfig:/root/.gitconfig:ro \
+  -v /tmp/run_script_entire_rosdistro/update_downstream_packages.py:/root/update_downstream_packages.py \
+  -ti update_downstream
